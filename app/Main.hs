a82c03eb0ae4556fea24ca80a720c51aee7358e5
SeanDurban <seandurban10@gmail.com>
2017-12-06 23:59:18 +0000

    added redcodePrograms and file I/O

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ import Control.Concurrent
 import Data.Sequence
 
 main = do
+  redcodeProg <- readFile "./redcodeProg/Imp.txt"
+  let x = lines redcodeProg
+  putStrLn $ show x
   let core = fromList [getCoreAddr 1, getCoreAddr 2]
   m <- newMVar $ core
   communicate 1 m
@@ -24,21 +27,28 @@ communicate n m = do
     putStrLn (show n ++ " received " ++ show seq1)
     putStrLn $ show n ++ " sending"
     putMVar m $ update 0 (getCoreAddr n) seq1
-
+--MARS simulator data types
 data Core = Core (Seq CoreAddr)
   deriving (Show,Read)
 
 data CoreAddr = CoreAddr Instr Field Field
   deriving (Show, Read)
 
-data Instr = DAT | MOV | ADD
+data Instr = DAT | MOV | ADD | SUB | JMP
   deriving (Show, Read)
 
-data Field = AddrMode Int | Empty
+data Field = Field AddrMode Int | Empty
   deriving (Show, Read)
 
-data AddrMode = Immed| Indiret -- | # | < | Empty
+data AddrMode = Immed | Indirect  -- | # | < | Empty
   deriving (Show, Read)
 
 getCoreAddr:: Int -> CoreAddr
-getCoreAddr n = CoreAddr DAT Empty $ AddrMode n
+getCoreAddr n = CoreAddr DAT Empty $ Field Indirect n
+
+--redcode data types
+
+data Prog = Prog Int [Move]
+  deriving (Show, Read)
+data Move = Move Instr Field Field
+  deriving (Show, Read)
