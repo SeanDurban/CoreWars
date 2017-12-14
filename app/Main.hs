6933b404a81c5fb525db62e17c1d73564808f805
SeanDurban <seandurban10@gmail.com>
2017-12-14 16:48:21 +0000

    function to execute instructions

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Data.Sequence
 import Control.Monad
 
 main = do
-  redcodeProg <- readFile "./redcodeProg/Imp.txt"
+  redcodeProg <- readFile "./redcodeProg/Dwarf.txt"
   let progIn = lines redcodeProg
   let input = Prelude.filter stripComments progIn
   let inputFiltered = map (Prelude.filter (/= ',')) input
@@ -47,16 +47,13 @@ runRedcode pNo coreMvar indexMvar= do
       core <- takeMVar coreMvar
       curI <- takeMVar indexMvar
       putStrLn $ "Prog"++show pNo ++ " acquired core\n"
-    --  instruction <- readInstruction i pNo core
-    --  let i= execute instruction core
-      let curInstr = index core (curI `mod` 15)
-      putStrLn $ " Instruction at i " ++ show curInstr ++ "\n"
-  --    let updatedCore = executeInstr core curI
-      let index2 = (curI+1) `mod` 15
-      test coreMvar $ update 0 sampleCoreAddr core
-    --  putMVar coreMvar $ update 0 sampleCoreAddr core
-      putMVar indexMvar index2
-      putStrLn $ "Prog"++show pNo ++ " released core " ++ show index2 ++ "\n"
+      let curCoreAddr = index core (curI `mod` 15)
+      putStrLn $ " Instruction at i " ++ show curCoreAddr ++ "\n"
+      let updatedCore = executeInstr core curCoreAddr curI
+      let updatedIndex = getNewIndex curCoreAddr curI
+      putMVar coreMvar updatedCore
+      putMVar indexMvar updatedIndex
+      putStrLn $ "Prog"++show pNo ++ " released core, new index:  " ++ show updatedIndex ++ "\n"
       --release core and sleep to avoid starvation
       threadDelay(10^7) --could make this random
 
@@ -69,22 +66,105 @@ applyAddrMode (Field Indirect n) curI core =  --want to go get value of that add
   let
     instr = index core (curI+n)  --get coreInstr at curI +n
     instrVal = 0 --val = extractVal instr
-  in curI + instrVal
+    in curI + instrVal
 applyAddrMode (Field Direct n) curI core = curI + n
 applyAddrMode (Field AutoDec n) curI core = --n and treated as indirect then decr value at n
   let
     instr = index core (curI+n)  --get coreInstr at curI +n
     instrVal = 0 --val = extractVal instr -1
-  in curI + instrVal
+    in curI + instrVal
 
-executeInstr :: Seq CoreAddr -> Int -> Seq CoreAddr
-executeInstr core curI
-  | coreAddr == MOV = update fieldA (index core curI) core
-  | otherwise = core
-  where oldCoreAddr = index core curI
-        coreAddr = extractInstr oldCoreAddr
-        fieldA = extractAField oldCoreAddr
-        fieldB = Empty
+getNewIndex :: CoreAddr -> Int -> Int
+getNewIndex (CoreAddr JMP (Field addrMode aVal) Empty) curI =  (curI + aVal) `mod` 15
+getNewIndex (CoreAddr JMZ (Field addMode1 aVal) (Field addMode2 bVal)) curI =
+    case bVal of
+      0 -> (curI + aVal) `mod` 15
+      _ -> (curI) `mod` 15
+getNewIndex (CoreAddr JMN (Field addMode1 aVal) (Field addMode2 bVal)) curI =
+    case bVal of
+      0 -> (curI) `mod` 15
+      _ -> (curI + aVal) `mod` 15
+getNewIndex (CoreAddr CMP (Field addMode1 aVal) (Field addMode2 bVal)) curI =
+    case (aVal - bVal) of
+      0 -> (curI+2) `mod` 15
+      _ -> (curI + 1) `mod` 15
+getNewIndex _ curI =
+  curI + 1
+--need DJN CMP
+
+-- CoreAddr Instr Field Field
+--Field AddrMode Int
+executeInstr :: Seq CoreAddr -> CoreAddr -> Int -> Seq CoreAddr
+executeInstr core (CoreAddr MOV (Field Immed aVal) b) curIndex =
+  let
+    bIndex = applyAddrMode b curIndex core
+    newBCoreAddr = CoreAddr DAT (Field Immed aVal) Empty
+    in (update bIndex newBCoreAddr core)
+executeInstr core (CoreAddr MOV a b) curIndex =
+  let
+    aIndex = applyAddrMode a curIndex core
+    bIndex = applyAddrMode b curIndex core
+    aCoreAddr  = index core aIndex
+    in (update bIndex aCoreAddr core)
+executeInstr core (CoreAddr ADD (Field Immed aVal) b) curIndex =
+  let
+    bIndex = applyAddrMode b curIndex core
+    bCoreAddr  = index core bIndex
+    newBCoreAddr = updateCoreVal ADD bCoreAddr 0 aVal
+    in (update bIndex newBCoreAddr core)
+
+executeInstr core (CoreAddr ADD a b) curIndex =
+  let
+    aIndex = applyAddrMode a curIndex core
+    bIndex = applyAddrMode b curIndex core
+    bCoreAddr  = index core bIndex
+    aCoreAddr  = index core aIndex
+    n1 = extractAVal aCoreAddr
+    n2 = extractBVal bCoreAddr
+    newBCoreAddr = updateCoreVal ADD bCoreAddr n1 n2
+    in (update bIndex newBCoreAddr core)
+
+executeInstr core (CoreAddr SUB (Field Immed aVal) b) curIndex =
+  let
+    bIndex = applyAddrMode b curIndex core
+    bCoreAddr  = index core bIndex
+    newBCoreAddr = updateCoreVal SUB bCoreAddr 0 aVal
+    in (update bIndex newBCoreAddr core)
+
+executeInstr core (CoreAddr SUB a b) curIndex =
+  let
+    aIndex = applyAddrMode a curIndex core
+    bIndex = applyAddrMode b curIndex core
+    bCoreAddr  = index core bIndex
+    aCoreAddr  = index core aIndex
+    n1 = extractAVal aCoreAddr
+    n2 = extractBVal bCoreAddr
+    newBCoreAddr = updateCoreVal SUB bCoreAddr n1 n2
+    in (update bIndex newBCoreAddr core)
+
+--All other instr don't change the core
+-- JMP JMZ JMN DJN CMP
+executeInstr core _ _ = core
+
+
+extractAVal :: CoreAddr -> Int
+extractAVal (CoreAddr instr (Field _ val) b) = val
+extractAVal (CoreAddr instr (Empty) b) = 0
+
+extractBVal :: CoreAddr -> Int
+extractBVal (CoreAddr instr a (Field _ val)) = val
+extractBVal (CoreAddr instr a (Empty)) = 0
+
+
+--Will need to add cases here to deal with Empty Fields
+updateCoreVal :: Instr -> CoreAddr -> Int -> Int -> CoreAddr
+--updateCoreVal ADD (CoreAddr instr a (Field adrMode bVal)) _ n2 =
+--  CoreAddr instr a (Field adrMode ((bVal + n2) `mod` 15))
+updateCoreVal ADD (CoreAddr instr (Field aAdrMode aVal) (Field bAdrMode bVal)) n1 n2 =
+  CoreAddr instr (Field aAdrMode ((aVal + n1) `mod` 15)) (Field bAdrMode ((bVal + n2) `mod` 15))
+updateCoreVal SUB (CoreAddr instr (Field aAdrMode aVal) (Field bAdrMode bVal)) n1 n2 =
+  CoreAddr instr (Field aAdrMode ((aVal - n1) `mod` 15)) (Field bAdrMode ((bVal - n2) `mod` 15))
+
 
 extractAField :: CoreAddr -> Int
 --extractAField _ (Field Direct n) _ = n
