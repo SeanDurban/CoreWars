b5e78c988b1bc91521322087f6835bc02fc61a80
SeanDurban <seandurban10@gmail.com>
2017-12-14 01:47:33 +0000

    starting on mars core

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Control.Concurrent
 import Data.Sequence
+import Control.Monad
 
 main = do
-  redcodeProg <- readFile "./redcodeProg/Dwarf.txt"
+  redcodeProg <- readFile "./redcodeProg/Imp.txt"
   let progIn = lines redcodeProg
   let input = Prelude.filter stripComments progIn
   let inputFiltered = map (Prelude.filter (/= ',')) input
   let moves =  map words inputFiltered
   putStrLn $ show moves
-  let core = fromList [getCoreAddr 1, getCoreAddr 2]
-  m <- newMVar $ core
---  communicate 1 m
-  --communicate 2 m
-  --communicate 3 m
-  communicate 4 m
-  communicate 0 m
-  threadDelay (10^5)
+  let dwarf = redcodeParser moves 1
+  putStrLn $ show dwarf
+  let core = fromList $ map (getCoreAddr) [0..15]
+  let core2 = update 1 (CoreAddr MOV (Field Direct 0) (Field Direct 1) ) core
+  m <- newMVar $ core2
+
+  index <- newMVar $ 1
+  index2 <- newMVar $ 46
+  index3 <- newMVar $ 38
+  runRedcode 1 m index
+  --runRedcode 2 m index2
+  --runRedcode 3 m index3
+
+  threadDelay (10^8)
+  --putStrLn $ show core
+
 --end main
 
 communicate n m = do
@@ -25,6 +34,51 @@ communicate n m = do
     putStrLn (show n ++ " received " ++ show seq1)
     putStrLn $ show n ++ " sending"
     putMVar m $ update 0 (getCoreAddr n) seq1
+
+
+--runRedcode (Prog pNo i moves:[]) = runProg pNo i
+
+--runRedcode :: Prog -> MVar
+--runRedcode pNo i moves coreMvar= do
+runRedcode pNo coreMvar indexMvar= do
+  forkIO $ do
+    forever $ do
+    --attempt to secure Core
+      core <- takeMVar coreMvar
+      curI <- takeMVar indexMvar
+      putStrLn $ "Prog"++show pNo ++ " acquired core\n"
+    --  instruction <- readInstruction i pNo core
+    --  let i= execute instruction core
+      let curInstr = index core curI
+      putStrLn $ " Instruction at i " ++ show curInstr ++ "\n"
+      let updatedCore = executeInstr core curI
+      let index2 = (curI+1) `mod` 15
+      putMVar coreMvar $ update 0 sampleCoreAddr core
+      putMVar indexMvar index2
+      putStrLn $ "Prog"++show pNo ++ " released core " ++ show index2 ++ "\n"
+      --release core and sleep to avoid starvation
+      threadDelay(10^7) --could make this random
+
+executeInstr :: Seq CoreAddr -> Int -> Seq CoreAddr
+executeInstr core curI
+  | coreAddr == MOV = update fieldA (index core curI) core
+  | otherwise = core
+  where oldCoreAddr = index core curI
+        coreAddr = extractInstr oldCoreAddr
+        fieldA = extractAField oldCoreAddr
+        fieldB = Empty
+
+extractAField :: CoreAddr -> Int
+extractAField _ (Field Direct n) _ = n
+extractAField _ _ _ = -1
+
+extractInstr :: CoreAddr -> Instr
+extractInstr MOV a b = MOV
+extractInstr _ _ _ = DAT
+
+sampleCoreAddr = CoreAddr DAT Empty $ Field Indirect 3
+
+sampleImp =Prog 1 0 [Move MOV (Field Direct 0) (Field Direct 1)]
 
 stripComments:: String -> Bool
 stripComments "" = False
@@ -45,17 +99,45 @@ movesParser (move:(moves)) =
 
 moveParser :: [String] -> Move
 moveParser move
-  | instr == "DAT" = Move DAT fieldA fieldB
+  | instr == "DAT" = Move DAT Empty fieldA
   | instr == "ADD" = Move ADD fieldA fieldB
+  | instr == "SUB" = Move SUB fieldA fieldB
+  | instr == "MOV" = Move MOV fieldA fieldB
+  | instr == "JMP" = Move JMP fieldA Empty
+  | instr == "SPL" = Move SPL fieldA Empty
+  | otherwise = Move DAT fieldA fieldB
   where
     instr = head move
-    fieldA = fieldAParser ""
-    fieldB = fieldBParser ""
+    fieldA = fieldAParser move
+    fieldB = fieldBParser move
 
-fieldAParser :: String -> Field
-fieldAParser _ = Empty
-fieldBParser :: String -> Field
-fieldBParser _ = Empty
+fieldAParser :: [String] -> Field
+fieldAParser move =
+  Field addrMode $ read fieldVal
+  where  f= move!!1
+         addrMode = addrModeParser $ head f
+         fieldVal = fieldValParser addrMode f
+
+fieldBParser :: [String] -> Field
+fieldBParser move=
+  Field addrMode $ read fieldVal
+  where  f= last move
+         addrMode = addrModeParser $ head f
+         fieldVal = fieldValParser addrMode f
+
+fieldValParser :: AddrMode -> String -> String
+fieldValParser Direct field
+  | head field == '@' = tail field
+  | otherwise = field
+fieldValParser _ field = tail field
+
+addrModeParser :: Char -> AddrMode
+addrModeParser '<' = AutoDec
+addrModeParser '@' = Indirect
+addrModeParser '#' = Immed
+addrModeParser '$' = Direct
+addrModeParser _ = Direct --Can not specify any addmode if Direct. this could be a sign or a num
+
 
 --MARS simulator data types
 data Core = Core (Seq CoreAddr)
@@ -64,17 +146,17 @@ data Core = Core (Seq CoreAddr)
 data CoreAddr = CoreAddr Instr Field Field
   deriving (Show, Read)
 
-data Instr = DAT | MOV | ADD | SUB | JMP
+data Instr = DAT | MOV | ADD | SUB | JMP | JMZ | JMN | DJN | CMP | SPL
   deriving (Show, Read)
 
 data Field = Field AddrMode Int | Empty
   deriving (Show, Read)
 
-data AddrMode = Immed | Indirect  -- | # | < | Empty
+data AddrMode = Immed | Indirect | Direct | AutoDec -- | # | < | Empty
   deriving (Show, Read)
 
 getCoreAddr:: Int -> CoreAddr
-getCoreAddr n = CoreAddr DAT Empty $ Field Indirect n
+getCoreAddr n = CoreAddr ADD (Field Direct 0) (Field Direct 1)
 
 --redcode data types
 --First in is the progNo
